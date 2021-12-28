95fe3b45331367a768553d1087483a6de6487c53
Nian-Hsuan Tsai <nhtsai@Nian-Hsuans-MacBook-Pro.local>
2021-12-28 10:59:57 -0500

    update count naming convention

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "aws_spot_instance_request" "aws_dl_custom_spot" {
 
   # Workaround to make sure the spot request tags are propogated down to the instance itself.
   provisioner "local-exec" {
-    command = join("", formatlist("aws ec2 create-tags --resources ${self.spot_instance_id} --tags Key=\"%s\",Value=\"%s_${count.index}\"; ", keys(self.tags), values(self.tags)))
+    command = join("", formatlist("aws ec2 create-tags --resources ${self.spot_instance_id} --tags Key=\"%s\",Value=\"%s-${count.index}\"; ", keys(self.tags), values(self.tags)))
     # No need to add since using env variables directly.
     # environment = {
     #   AWS_ACCESS_KEY_ID     = var.aws_access_key
